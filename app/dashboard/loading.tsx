eb8e0cdb85058b5126abd40e70522a121fd810d5
zexxR2 <speakengwithmee@gmail.com>
2026-05-03 18:04:42 +0100

    fix login route

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <div className="p-10">Loading...</div>;
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,7 @@
 export default function LoginPage() {
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-semibold">
-        Login
-      </h1>
+      <h1 className="text-3xl font-bold">Login</h1>
     </div>
   );
 }
